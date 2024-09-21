b06ad729697000ac0daf154dd0b8e1113cca6ed7
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-09-21 10:31:13 +0930

    feat: navbar with navlinks

--- src/App_ComponentRouter.js
+++ src/App_ComponentRouter.js
@@ -1,15 +1,22 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import '../styles/index.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
+
     <div className="App">
+
       <BrowserRouter>
+
+      {/* This is our custom navbar */}
+      <Navbar />
+
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/about' element={<AboutPage />} />
